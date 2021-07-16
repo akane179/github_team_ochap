d10078b4e72632621e5cfdbb5ad0bd24d431f919
akane179 <akane-110516-hara@ezweb.ne.jp>
2021-07-16 22:14:44 +0900

    miss

--- src/index.js
+++ src/index.js
@@ -128,8 +128,12 @@
 	/* 演習の編集範囲 はじめ */
 
 	/* 演習1 ここから */
+
 	function sub()
-	function add()
+    function add(){
+	eqlNum.value = parseFloat(midNum[0].value) + parseFloat(midNum[1].value);
+    }
+
 	/* 演習1 ここまで */
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
